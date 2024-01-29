f6d5ef9e8b1c2beae0cbceb06d16333793d77a50
Germen Wong <1953792796@qq.com>
2024-01-29 16:55:05 +0800

    完成沉浸式状态栏

--- app/src/main/java/com/hgm/newsapp/MainActivity.kt
+++ app/src/main/java/com/hgm/newsapp/MainActivity.kt
@@ -5,17 +5,21 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.lifecycleScope
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.hgm.newsapp.core.navigation.NavGraph
 import com.hgm.newsapp.domain.use_case.AppEntryUseCase
 import com.hgm.newsapp.presentation.onboarding.OnBoardingScreen
@@ -40,9 +44,18 @@ class MainActivity : ComponentActivity() {
             installSplashScreen().apply {
                   viewModel.splashCondition
             }
-
             setContent {
                   NewsAppTheme {
+                        val isDarkTheme = isSystemInDarkTheme()
+                        val systemController = rememberSystemUiController()
+
+                        SideEffect {
+                              systemController.setSystemBarsColor(
+                                    color = Color.Transparent,
+                                    darkIcons =!isDarkTheme
+                              )
+                        }
+
                         Surface(
                               modifier = Modifier.fillMaxSize(),
                               color = MaterialTheme.colorScheme.background
